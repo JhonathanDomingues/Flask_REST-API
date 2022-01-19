834ff8c693bdca206805485f7d609abfb7aa2176
JhonathanDomingues <domingues.jhonathan@gmail.com>
2022-01-19 14:30:15 -0300

    master

--- Projeto_API_flask/models/hotel.py
+++ Projeto_API_flask/models/hotel.py
@@ -10,7 +10,7 @@ class HotelModel(banco.Model):
 	estrelas = banco.Column(banco.Float(precision=1))
 	diaria = banco.Column(banco.Float(precision=2))
 	cidade = banco.Column(banco.String(40))
-	site_id = banco.Column(banco.Interger, banco.Foreignkey('sites.site_id'))
+	site_id = banco.Column(banco.Integer, banco.ForeignKey('sites.site_id'))
 	
 
 
