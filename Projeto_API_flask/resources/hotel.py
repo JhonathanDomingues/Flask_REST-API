c7006f4ea25917a9c2e95c3442259406709095c4
JhonathanDomingues <domingues.jhonathan@gmail.com>
2022-01-18 23:58:26 -0300

    master

--- Projeto_API_flask/resources/hotel.py
+++ Projeto_API_flask/resources/hotel.py
@@ -1,5 +1,6 @@
 from flask_restful import Resource, reqparse
 from models.hotel import HotelModel
+from flask_jwt_extended import jwt_required
 
 
 class Hoteis(Resource):
@@ -19,6 +20,7 @@ class Hotel(Resource):
 			return hotel.json()
 		return {'message': 'Hotel note found.'}, 404
 	
+	@jwt_required()
 	def post(self, hotel_id):
 		if HotelModel.find_hotel(hotel_id):
 			return {"message":f"Hotel id '{hotel_id}' already exists"}, 400
@@ -31,7 +33,7 @@ class Hotel(Resource):
 			return {'message': 'An internal error ocurred trying to save hotel.'}, 500
 		return hotel.json()
 
-
+	@jwt_required()
 	def put(self, hotel_id):
 
 		dados =  Hotel.argumentos.parse_args()
@@ -50,7 +52,7 @@ class Hotel(Resource):
 			return {'message': 'An internal error ocurred trying to save hotel.'}, 500
 		return hotel.json(), 201
 		
-
+	@jwt_required()
 	def delete(self, hotel_id):
 		hotel = HotelModel.find_hotel(hotel_id)
 		if hotel:
