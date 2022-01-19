631cf10f9c39287d0de4c5eb156863c423bf6894
JhonathanDomingues <domingues.jhonathan@gmail.com>
2022-01-18 22:44:56 -0300

    master

--- Projeto_API_flask/models/hotel.py
+++ Projeto_API_flask/models/hotel.py
@@ -1,3 +1,4 @@
+from sqlalchemy import delete
 from sql_alchemy import banco
 
 
@@ -45,7 +46,10 @@ class HotelModel(banco.Model):
 		self.estrelas = estrelas
 		self.diaria = diaria
 		self.cidade = cidade
-		
+
+	def delete_hotel(self):
+		banco.session.delete(self)
+		banco.session.commit()
 
 
 
--- Projeto_API_flask/resources/hotel.py
+++ Projeto_API_flask/resources/hotel.py
@@ -2,34 +2,10 @@ from email import message
 from flask_restful import Resource, reqparse
 from models.hotel import HotelModel
 
-hoteis = [
-	{
-		'hotel_id': 'alpha',
-		'nome': 'Alpha Hotel',
-		'estrelas': 4.3,
-		'diaria': 420.34,
-		'cidade': 'Rio de Janeiro'
-	},
-	{
-		'hotel_id': 'bravo',
-		'nome': 'Bravo Hotel',
-		'estrelas': 4.4,
-		'diaria': 380.90,
-		'cidade': 'Santa Catarina'
-	},
-	{
-		'hotel_id': 'charlie',
-		'nome': 'Charlie Hotel',
-		'estrelas': 3.9,
-		'diaria': 320.20,
-		'cidade': 'Santa Catarina'
-	}
-]
-
 
 class Hoteis(Resource):
 	def get(self):
-		return {'hoteis': hoteis}
+		return {'hoteis': [hotel.json() for hotel in HotelModel.query.all()]}
 
 class Hotel(Resource):
 	argumentos =  reqparse.RequestParser()
@@ -68,6 +44,8 @@ class Hotel(Resource):
 		
 
 	def delete(self, hotel_id):
-		global hoteis
-		hoteis = [hotel for hotel in hoteis if hotel['hotel_id'] != hotel_id]
-		return {'message': 'Hotel deleted.'}
+		hotel = HotelModel.find_hotel(hotel_id)
+		if hotel:
+			hotel.delete_hotel()
+			return {'message': 'Hotel deleted.'}
+		return {'menssage': 'Hotel not found.'}
